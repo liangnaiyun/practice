add4ce2658c7af78b1a296dc0b73412349c294d9
Liang Nai Yun <zzx51406@gmail.com>
2021-04-07 21:56:42 +0800

    延伸一：印出猜了幾次

--- random_num.py
+++ random_num.py
@@ -2,13 +2,16 @@
 # 讓使用者重複去猜數字
 # 猜對的話 印出 "終於猜對了！"
 # 猜錯的話 要告訴他 比答案大/小
+# 延伸一：印出猜了幾次
 
 import random
 
 answer = random.randint(1, 100)
+total_guest_count = 0
 while True:
 	user_guest_number = input("請輸入1~100數字：")
 	user_guest_number = int(user_guest_number)
+	total_guest_count = total_guest_count + 1	
 	if answer != user_guest_number:
 		if answer > user_guest_number:
 			print("比答案小")
@@ -16,4 +19,5 @@ while True:
 			print("比答案大")
 	else:
 		print("終於猜對了！")
+		print("共猜了", total_guest_count, "次！")
 		break
