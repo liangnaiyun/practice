377a0eeda07bed0750beee642fbca666de35e0cf
Liang Nai Yun <zzx51406@gmail.com>
2021-04-10 14:51:17 +0800

    讀取檔案資料，寫入檔案

--- products.py
+++ products.py
@@ -1,21 +1,26 @@
 import csv
 products = []
 
-while  True:
+#先讀取原本資料
+with open('products.csv', 'r', newline = '', encoding = 'utf-8') as f:
+	rows = csv.reader(f, delimiter = ',')
+	for row in rows:
+		if ['商品', '價格'] == row:
+			continue
+		print(row)
+
+#讓使用者輸入商品、價錢
+while True:
 	name = input('請輸入商品名稱：')
 	if name == 'q':
 		break
 	price = input('請輸入商品價格：')
 	products.append([name,price])
 
-print(products)
-
-for p in products:
-	print(p[0])
-	print(p[1])
-
+#寫入檔案
 #使用newline可以讓寫入資料的每一個不要多一行空白行
-with open('products.csv', 'w', newline = '') as f:
-	writer = csv.writer(f)
+with open('products.csv', 'a', newline = '', encoding = 'utf-8') as f:
+	writer = csv.writer(f, delimiter=',')
+	writer.writerow(['商品', '價格'])
 	for p in products:
 		writer.writerow([p[0], p[1]])
