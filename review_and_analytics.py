14f2a1f201c3037de6f76cb16ed7552570208bda
Liang Nai Yun <zzx51406@gmail.com>
2021-04-12 20:25:19 +0800

    使用pypi第三方套件使用progress bar

--- review_and_analytics.py
+++ review_and_analytics.py
@@ -3,18 +3,24 @@
 # 讀取完畢，顯示總共多少筆留言
 # 計算留言的平均長度
 # 篩選出長度小於100的留言
+import time
+from hive_progress_bar import ProgressBar
+
 
 data = []
 total_char_len = 0
 count = 0
 total_count = 0
-with open('review.txt', 'r') as f:
+pb = ProgressBar("waiting...", 1000000)
+with open('reviews.txt', 'r') as f:
 	for line in f:
+
 		count += 1
 		line = line.strip('\n')
 		data.append(line)
-		if count % 1000 == 0:
-			print(len(data))
+		# if count % 1000 == 0:
+		# 	print(len(data))
+		pb.next()
 
 total_count = len(data)
 print('檔案讀取完了，總共有', total_count, '筆資料')
@@ -42,15 +48,17 @@ print('一共有', len(filter_review_list2), '筆留言提到good')
 
 # list 快寫法
 # 篩選留言包含good
-good_list = [d for d in data if 'good' in d]
-print('一共有', len(good_list), '筆留言提到good')
+# good_list = [d for d in data if 'good' in d]
+# print('一共有', len(good_list), '筆留言提到good')
 
-bad = ['bad' in d for d in data]
-print(bad)
+# bad = ['bad' in d for d in data]
+# print(bad)
 
 
-#利用字典找到所有詞出現的次數
+#利用字典找到所有詞出現的次數，並記錄時間
+start_time = time.time()
 wc = {}
+
 for line in data:
 	#split()預設會以空白字元切割(包含連續空白)
 	sp = line.split()
@@ -60,8 +68,12 @@ for line in data:
 		else:
 			wc[s] = 1
 
-for w in wc:
-	print(w, "：", wc[w])	
+# for w in wc:
+# 	print(w, "：", wc[w])	
+
+end_time = time.time()
+
+print('總共花了', end_time - start_time, '秒')
 
 while True:
 	word = input('請問你想查甚麼字：')
